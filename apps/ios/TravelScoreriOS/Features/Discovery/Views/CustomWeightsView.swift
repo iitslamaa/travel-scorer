10ef856946084e1d36a7edc0b2918e040e584e5b
iitslamaa <smilesnfaces@gmail.com>
2026-02-22 19:13:39 -0500

    Refactor weight system to centralized ScoreWeightsStore, normalize scoring, and decouple cards from EnvironmentObject

---
+++ apps/ios/TravelScoreriOS/Features/Discovery/Views/CustomWeightsView.swift
@@ -0,0 +1,8 @@
+//
+//  CustomWeightsView.swift
+//  TravelScoreriOS
+//
+//  Created by Lama Yassine on 2/22/26.
+//
+
+import Foundation
---
+++ apps/ios/TravelScoreriOS/Models/ScoreWeights.swift
@@ -0,0 +1,8 @@
+//
+//  ScoreWeights.swift
+//  TravelScoreriOS
+//
+//  Created by Lama Yassine on 2/22/26.
+//
+
+import Foundation
